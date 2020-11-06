37f1cd139112f3d2148eb34395b8c0f02bb2e69b
Sebastian Clancy <foldit.com@gmail.com>
2020-11-06 15:06:53 -0800

    Adding the other functions (sets)

--- download.py
+++ download.py
@@ -1,6 +1,5 @@
 import requests
-
-
+# Client to download and do all the stuff
 
 class SmashGGConnectionTHINGY:
     def __init__(self, api_token):
@@ -47,12 +46,80 @@ class SmashGGConnectionTHINGY:
                 "variables": f'{{"videogameId": {self.id}, "page": {0}, "perPage": {per_page}, "after": {start_time}, "before": {end_time} }}',
             },
             headers = {
-                'Authorization': f'Bearer {self.api}'
+                'Authorization': f'Bearer {self.api}',
             }
         )
         return r.json()['data']['tournaments']['nodes']
+
     def get_tournament_by_id(self, id):
         r = requests.post(
-            self.url
-            
+            self.url,
+            data={
+                "query": """
+                    query TournamentsByVideogame($id: ID!) {
+                      tournament(id: $id) {
+                        id
+                        name
+                        endAt
+                        events(limit: 10) {
+                            name
+                            id
+                            videogame {
+                                id
+                            }
+                        }
+                      }
+                    }
+                """,
+                "variables": f'{{"id": {id} }}',
+            },
+            headers={
+                'Authorization': f'Bearer {self.api}',
+            }
         )
+        return r.json()['data']['tournament']
+
+    def get_sets(self, event_id, page=1):
+        r = requests.post(
+            self.url,
+            data={
+                "query": """
+                    query SetsByEvent($eventId: ID!, $page: Int!, $perPage: Int!) {
+                      event(id: $eventId) {
+                        sets(page: $page, perPage: $perPage) {
+                            nodes {
+                                id
+                                games {
+                                    winnerId
+                                    stage {
+                                      name
+                                    }
+                                    selections {
+                                        entrant {
+                                            id
+                                            name
+                                        }
+                                        selectionType
+                                        selectionValue
+                                    }
+                                }
+                            }
+                        }
+                      }
+                    }
+                """,
+                "variables": f'{{"eventId": {event_id}, "page": {page}, "perPage": {40} }}',
+            },
+            headers={
+                'Authorization': f'Bearer {self.api}',
+            }
+        )
+        try:
+            event = r.json()['data']['event']
+            if event is None:
+                return []
+            sets = event['sets']['nodes']
+        except Exception:
+            print(r.json())
+            raise
+        return sets
